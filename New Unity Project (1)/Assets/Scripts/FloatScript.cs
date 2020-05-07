7e174e002975b1ba0f0cd072fccb08005b32aefa
ihartog <26035@ma-web.nl>
2020-05-07 14:42:16 +0100

    started working on float physics

---
+++ New Unity Project (1)/Assets/Scripts/FloatScript.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatScript : MonoBehaviour
+{
+    [SerializeField]
+    private float airDrag = 1;
+    [SerializeField]
+    private float waterDrag = 10;
+    [SerializeField]
+    private Transform[] floatPoints;
+    private bool attachToSurface;
+    private bool affectDirection = true;
+
+    protected Rigidbody rb;
+    protected Waves waves;
+
+    protected float waterLine;
+    protected Vector3[] waterLinePoints;
+
+    protected Vector3 centerOffset;
+    protected Vector3 smoothVectorRotation;
+        protected Vector3 targetUp;
+    public Vector3 center { get { return transform.position + centerOffset; } }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        waves = FindObjectOfType<Waves>();
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+
+        waterLinePoints = new Vector3[floatPoints.Length];
+        for (int i = 0; i <floatPoints.Length; i++)
+        {
+            waterLinePoints[i] = floatPoints[i].position;
+            centerOffset = PhysicsHelper.GetCenter(waterLinePoints) - transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newWaterLine = 0f;
+        bool pointUnderWater = false;
+
+        for (int i = 0; i < floatPoints.Length; i++)
+        {
+            waterLinePoints[i] = floatPoints[i].position;
+            waterLinePoints[i].y = waves.GetHeight(floatPoints[i].position);
+            newWaterLine += waterLinePoints[i].y / floatPoints.Length;
+            if (waterLinePoints[i].y > floatPoints[i].position.y)
+            {
+                pointUnderWater = true;
+            }
+            var waterLineDelta = newWaterLine - waterLine;
+            waterLine = newWaterLine;
+
+            var gravity = Physics.gravity;
+            rb.drag = airDrag;
+            if (attachToSurface)
+            {
+                rb.position = new Vector3(rb.position.x, waterLine - centerOffset.y,  rb.position.z);
+            }
+
+            else
+            {
+                gravity = affectDirection ? targetUp * -Physics.gravity.y : -Physics.gravity;
+                transform.Translate(Vector3.up * waterLineDelta * 0.9f);
+            }
+            rb.AddForce(gravity * Mathf.Clamp(Mathf.Abs(waterLine - center.y), 0, 1));
+
+            targetUp = PhysicsHelper.GetNormal(waterLinePoints);
+
+            if (pointUnderWater)
+            {
+                targetUp = Vector3.SmoothDamp(transform.up, targetUp, ref smoothVectorRotation, 0.2f);
+                rb.rotation = Quaternion.FromToRotation(transform.up, targetUp) * rb.rotation;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        if (floatPoints == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < floatPoints.Length; i++)
+        {
+            if (floatPoints[i] == null)
+            {
+                continue;
+            }
+            if (waves != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawCube(waterLinePoints[i], Vector3.one * 0.3f);
+            }
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(floatPoints[i].position, 0.1f);
+        }
+
+        if (Application.isPlaying)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(new Vector3(center.x, waterLine, center.z), Vector3.one * 1f);
+        }
+    }
+}
--- New Unity Project (1)/Assets/Scripts/Waves.cs
+++ New Unity Project (1)/Assets/Scripts/Waves.cs
@@ -8,6 +8,8 @@ public class Waves : MonoBehaviour
     [SerializeField]
     private int dimensions = 10;
     public Octave[] octaves;
+    [SerializeField]
+    private float uvScale;
 
     protected MeshFilter meshFilter;
     protected Mesh mesh;
@@ -20,10 +22,45 @@ public class Waves : MonoBehaviour
 
         mesh.vertices = GenerateVerts();
         mesh.triangles = GenerateTries();
+        mesh.uv = GenerateUVs();
         mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
 
         meshFilter = gameObject.AddComponent<MeshFilter>();
         meshFilter.mesh = mesh;
+    }
+
+    public float GetHeight(Vector3 position)
+    {
+        var scale = new Vector3(1 / transform.lossyScale.x, 0, 1 / transform.lossyScale.z);
+        var localPos = Vector3.Scale((position - transform.position), scale);
+
+        var p1 = new Vector3(Mathf.Floor(localPos.x), 0, Mathf.Floor(localPos.z));
+        var p2 = new Vector3(Mathf.Floor(localPos.x), 0, Mathf.Ceil(localPos.z));
+        var p3 = new Vector3(Mathf.Ceil(localPos.x), 0, Mathf.Floor(localPos.z));
+        var p4 = new Vector3(Mathf.Ceil(localPos.x), 0, Mathf.Ceil(localPos.z));
+
+        p1.x = Mathf.Clamp(p1.x, 0, dimensions);
+        p1.z = Mathf.Clamp(p1.z, 0, dimensions);
+        p2.x = Mathf.Clamp(p2.x, 0, dimensions);
+        p2.z = Mathf.Clamp(p2.z, 0, dimensions);
+        p3.x = Mathf.Clamp(p3.x, 0, dimensions);
+        p3.z = Mathf.Clamp(p3.z, 0, dimensions);
+        p4.x = Mathf.Clamp(p4.x, 0, dimensions);
+        p4.z = Mathf.Clamp(p4.z, 0, dimensions);
+
+        float max = Mathf.Max(Vector3.Distance(p1, localPos), Vector3.Distance(p2, localPos), Vector3.Distance(p3, localPos), Vector3.Distance(p4, localPos) + Mathf.Epsilon);
+        float dist = (max - Vector3.Distance(p1, localPos)) 
+                     + (max - Vector3.Distance(p2, localPos)) 
+                     + (max - Vector3.Distance(p3, localPos)) 
+                     + (max = Vector3.Distance(p4, localPos));
+
+        float height = mesh.vertices[index(p1.x, p1.z)].y * (max - Vector3.Distance(p1, localPos)) 
+                     + mesh.vertices[index(p2.x, p2.z)].y * (max - Vector3.Distance(p2, localPos)) 
+                     + mesh.vertices[index(p3.x, p3.z)].y * (max - Vector3.Distance(p3, localPos)) 
+                     + mesh.vertices[index(p4.x, p4.z)].y * (max - Vector3.Distance(p4, localPos));
+
+        return height * transform.lossyScale.y / dist;
     }
 
     private int[] GenerateTries()
@@ -45,6 +82,22 @@ public class Waves : MonoBehaviour
         return tries;
     }
 
+    private Vector2[] GenerateUVs()
+    {
+        var uvs = new Vector2[mesh.vertices.Length];
+
+        for (int x = 0; x <= dimensions; x++)
+        {
+            for (int z = 0; z <= dimensions; z++)
+            {
+                var vec = new Vector2((x / uvScale) % 2, (z / uvScale) % 2);
+                uvs[index(x, z)] = new Vector2(vec.x <= 1 ? vec.x : 2 - vec.x, vec.y <= 1 ? vec.y : 2 - vec.y);
+            }
+        }
+
+        return uvs;
+    }
+
     private Vector3[] GenerateVerts()
     {
         var verts = new Vector3[(dimensions + 1) * (dimensions + 1)];
@@ -60,9 +113,9 @@ public class Waves : MonoBehaviour
         
     }
 
-    private int index(int x, int z)
+    private int index(float x, float z)
     {
-        return x * (dimensions + 1) + z;
+        return (int)( x * (dimensions + 1) + z);
     }
 
     // Update is called once per frame
@@ -73,19 +126,25 @@ public class Waves : MonoBehaviour
         {
             for (int z = 0; z <= dimensions; z++)
             {
-                var y = 0f;
+                float y = 0f;
                 for (int o = 0; o < octaves.Length; o++)
                 {
                     if (octaves[o].alternate)
                     {
-                        var perl = Mathf.PerlinNoise((x * octaves[o].scale.x) / dimensions, (z * octaves[o].scale.y) / dimensions) * Mathf.PI * 2f;
-                        y += Mathf.Cos(octaves[o].speed.magnitude * Time.time) * octaves[o].height;
+                        float perl = Mathf.PerlinNoise((x * octaves[o].scale.x) / dimensions, (z * octaves[o].scale.y) / dimensions) * Mathf.PI * 2f;
+                        y += Mathf.Cos(perl + octaves[o].speed.magnitude * Time.time) * octaves[o].height;
+                    }
+                    else
+                    {
+                        float perl = Mathf.PerlinNoise((x * octaves[o].scale.x + Time.time * octaves[o].speed.x) / dimensions, (z * octaves[o].scale.y + Time.time * octaves[o].speed.y) / dimensions) - 0.5f;
+                        y += perl * octaves[o].height;
                     }
                 }
                 verts[index(x, z)] = new Vector3(x, y, z);
             }
         }
         mesh.vertices = verts;
+        mesh.RecalculateNormals();
     }
 
     [Serializable]
