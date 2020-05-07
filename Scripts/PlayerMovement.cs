00dc2e9c71bed396a850d2702f7fafe45aa0c1d8
JustinvdLans <42778559+JustinvdLans@users.noreply.github.com>
2020-05-07 12:43:35 +0200

    Add files via upload

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    [SerializeField]
+    float speed = .1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        GetComponent<Rigidbody>().MovePosition(transform.position + dir * speed);
+    }
+}
