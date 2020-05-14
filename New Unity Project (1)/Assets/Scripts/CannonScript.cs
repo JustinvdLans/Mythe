b95802043c5b1d32182e45cdfd35cb37a8ae6cd5
ihartog <26035@ma-web.nl>
2020-05-14 08:27:04 +0100

    cannon fix

--- New Unity Project (1)/Assets/Scripts/CannonScript.cs
+++ New Unity Project (1)/Assets/Scripts/CannonScript.cs
@@ -8,13 +8,19 @@ public class CannonScript : MonoBehaviour
     private Transform firePos;
     [SerializeField]
     private GameObject cannonball;
+    private float timer = 1.5f;
 
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Instantiate(cannonball, firePos.position, Quaternion.identity);
+            if (timer <= 0)
+            {
+                Instantiate(cannonball, firePos.position, Quaternion.identity);
+                timer = 1.5f;
+            }
         }
     }
 }
