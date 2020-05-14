09cc3265f92d2298dd64248c62ef13b8c9651e05
ihartog <26035@ma-web.nl>
2020-05-14 08:47:17 +0100

    Revert "updates"
    This reverts commit ef9a3324a1802cf1e56815cf4f981d5ee62e5b94.

--- New Unity Project (1)/Assets/Scripts/CannonScript.cs
+++ New Unity Project (1)/Assets/Scripts/CannonScript.cs
@@ -8,20 +8,13 @@ public class CannonScript : MonoBehaviour
     private Transform firePos;
     [SerializeField]
     private GameObject cannonball;
-    private float timer = 1.5f;
 
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (timer <= 0)
-            {
-                Instantiate(cannonball, firePos.position, Quaternion.identity);
-                timer = 1.5f;
-            }
-            
+            Instantiate(cannonball, firePos.position, Quaternion.identity);
         }
     }
 }
