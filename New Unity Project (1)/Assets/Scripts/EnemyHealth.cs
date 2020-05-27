84c1ff90ed03e95663bb6aa8aac2c0716cf5a748
ihartog <26035@ma-web.nl>
2020-05-27 10:42:38 +0100

    enemy health bar added

---
+++ New Unity Project (1)/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    private float health = 100f;
+    private float currentHealth;
+    private Image enemyBar;
+    public float damage = 10f;
+    public Image EnemyBar { get { return enemyBar; } set { enemyBar = value; } }
+    [SerializeField]
+    private GameObject enemyUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyBar = enemyUI.GetComponent<Image>();
+        currentHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyBar.fillAmount = currentHealth / health;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            currentHealth -= damage;
+        }
+    }
+}
